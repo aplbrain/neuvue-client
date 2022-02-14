017293228388f0d7fb6a0afe9f40b6f696f897a7
vrose9@jhu.edu <vrose9@jhu.edu>
2022-02-14 14:38:49 -0500

    remove changes from client

--- neuvueclient/__init__.py
+++ neuvueclient/__init__.py
@@ -812,14 +812,6 @@ class NeuvueQueue:
                 old_metadata = self.get_task(task_id)['metadata']
                 value.update(old_metadata)
 
-            if key == 'duration':
-                old_duration = self.get_task(task_id)['duration']
-                value += old_duration
-                
-            if key == 'opened':
-                old_opened_time = self.get_task(task_id)['opened']
-                value = old_opened_time
-
             stri = f"/tasks/{task_id}/{key}"
             res = self._try_request( 
                 lambda: requests.patch(
